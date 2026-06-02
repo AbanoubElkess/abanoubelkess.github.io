6049e28e3d904481100b41dae7452a3061efa6a4
AbanoubElkess <AbanoubElkess@users.noreply.github.com>
2026-06-02 18:51:13 +0000

    Deploying to gh-pages from @ AbanoubElkess/abanoubelkess.github.io@5d712e18ce7c72923b0b2d03a7863a900acf252b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,6 +30,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
+        },{id: "nav-activity",
+          title: "Activity",
+          description: "Recent professional updates, academic milestones, and research presentations.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/activity/";
+          },
         },{id: "nav-cv",
           title: "CV",
           description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
